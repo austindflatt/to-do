30db98d0cef65706a4f7008c296afb6ff1285080
austindflatt <austindflatt@gmail.com>
2021-11-12 20:46:54 -0500

    update

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 const form = document.querySelector("#todoForm")
 const todo = document.querySelector("#todo-input");
-const deleteAll = document.querySelectorAll("#destroy")
+const deleteAll = document.querySelector("#destroy")
 
 form.addEventListener('click', function (event) {
     event.preventDefault();
     const ul = document.getElementById("list");
     const li = document.createElement("li");
     if (todo.value.length >= 4) {
-        li.appendChild(document.createTextNode(todo.value));
+        li.appendChild(document.createTextNode(`❗️${todo.value}`));
         ul.appendChild(li);
     } else {
-        alert("Error! You must enter a valid input")
+        alert("❌ Error! You must enter at least 4 or more characters.")
     }
     todo.value = '';
     console.log(todo.value);
